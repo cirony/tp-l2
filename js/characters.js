2bfec5910c30224050f5584c0c9413524ee57a99
Pereira Sanchez, K <k.pereira.sanchez@accenture.com>
2020-09-24 16:04:52 -0300

    responsive

--- js/characters.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".avatar").click(function() {
-        $(".selected").removeClass("selected");
-        $("#" + $(this).attr("target")).addClass("selected");
-        $(this).addClass("selected");
-    });
-});
--- js/default.js
+++ js/default.js
@@ -1,32 +1,71 @@
 $(document).ready(function() {
     /* Utilidad para hacer la barra Sticky*/
-    $('.status').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('.navigation').addClass('sticky');
 
-        } else {
-            $('.navigation').removeClass('sticky');
+    if ($('.status').length > 0) {
+        $('.status').waypoint(function(direction) {
+            if (direction == 'down') {
+                $('.navigation').addClass('sticky');
+
+            } else {
+                $('.navigation').removeClass('sticky');
 
 
-        }
-    }, {
-        offset: '100px'
-    });
+            }
+        }, {
+            offset: '100px'
+        });
+    }
 
     /* Mobile Nav - sacar si no se usa*/
     $('.js--mobile-nav-icon').click(function() {
         var nav = $('.js--main-nav');
-        var icon = $('.js--mobile-nav-icon i');
+        var icon = $('.js--mobile-nav-icon');
 
-        if (icon.hasClass('ion-navicon-round')) {
+        if (icon.hasClass('fa-bars')) {
             nav.slideDown(200);
-            icon.removeClass('ion-navicon-round');
-            icon.addClass('ion-close-round');
+            icon.removeClass('fa-bars');
+            icon.addClass('fa-times');
         } else {
             nav.slideUp(200);
-            icon.removeClass('ion-close-round');
-            icon.addClass('ion-navicon-round');
+            icon.removeClass('fa-times');
+            icon.addClass('fa-bars');
         }
 
+    });
+
+    $(document).ready(function() {
+        $(".avatar").click(function() {
+            $(".selected").removeClass("selected");
+            $("#" + $(this).attr("target")).addClass("selected");
+            $(this).addClass("selected");
+        });
+    });
+
+    $("body").click(function() {
+        $(".success").hide("fast")
+    });
+
+    $("#join-form").change(function() {
+        $(".error").hide();
+    });
+
+
+    $("#join-form").submit(function(event) {
+        event.preventDefault();
+
+        if ($(this).find("input[name='password']").val() != $(this).find("input[name='password2']").val()) {
+            console.log("pass invalida");
+            $(".error").show("fast");
+        } else {
+            $(".success span").text("¡Todo listo! Enviamos un correo a tu casilla " + $(this).find("input[name='email']").val() + " con los datos del registro.");
+            $(".success").show();
+        }
+
+    });
+
+    $("#newsletter").submit(function(event) {
+        event.preventDefault();
+        $(".success span").text("¡Gracias por suscribirte! Estarás recibiendo novedades proximamente en " + $(this).find("input[name='email']").val() + ".");
+        $(".success").show();
     });
 });
